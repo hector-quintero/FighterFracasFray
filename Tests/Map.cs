b4897294613f8b5577569f213a16865f23f7e90f
hquintero-1337 <hector.e.quintero@gmail.com>
2023-03-14 23:36:56 -0500

    tests

--- Tests/Map.cs
+++ Tests/Map.cs
@@ -1,1 +1,22 @@
-import 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+{
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestMethod]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            bool result = _primeService.IsPrime(1);
+
+            Assert.IsFalse(result, "1 should not be prime");
+        }
+    }
+}
